0ef39f75ccdebdd71537ed3f14e578cc0a844f6f
GJ713932 <116373991+GJ713932@users.noreply.github.com>
2022-12-20 10:29:53 -0800

    Started scrolling backrounds
    finished animations

--- FlappyBirdFinaljonassonP2/Assets/Scripts/BirbScript.cs
+++ FlappyBirdFinaljonassonP2/Assets/Scripts/BirbScript.cs
@@ -2,16 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class BirbScript : MonoBehaviour
 {
     public float upForce = 200f;
 
     private bool isDead = false;
     private Rigidbody2D rb2d;
+    private Animator anim;
+
 
     // Start is called before the first frame update
     void Start()
     {
+        anim = GetComponent<Animator>();
         rb2d = GetComponent<Rigidbody2D> ();
     }
 
@@ -22,6 +26,8 @@ public class BirbScript : MonoBehaviour
         {
             if (Input.GetMouseButtonDown (0))
             {
+                
+                anim.SetTrigger("Flap");
                 rb2d.velocity = Vector2.zero;
                 rb2d.AddForce(new Vector2(0, upForce));
 
@@ -32,6 +38,9 @@ public class BirbScript : MonoBehaviour
     void OnCollisionEnter2D(Collision2D other)
     {
         isDead = true;
+        
+        anim.SetTrigger("Die");
+        GameControl.instance.BirdDied();
     }
 
 }
